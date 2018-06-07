b03af64d39b2e5de757c3180096a2d5e9c3aa866
rfaerbz1 <bfaerber@cox.net>
2018-06-07 21:29:03 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,10 @@
 # dog.rb
+class Dog 
+  attr_accessor :fido, :snoopy,:lassie
+  
+  initialize(fido, snoopy, lassie)
+    @fido = fido
+    @snoopy = snoopy 
+    @lassie = lassie
+  end
+end
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,9 @@
 # person.rb
+class Person 
+  attr_accessor :adele_goldberg, :alan_kay
+  
+  initialize(adele_goldberg, alan_kay)
+    @adele_goldberg = adele_goldberg
+    @alan_kay = alan_kay
+  end
+end 
